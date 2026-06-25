0cd693f88e7188632e541a9669660798d4696b6f
ujusa12 <114237075+ujusa@users.noreply.github.com>
2026-06-25 08:44:53 +0530

    Update test.ts

--- pokemon-ui/src/test.ts
+++ pokemon-ui/src/test.ts
@@ -12,12 +12,12 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 
-// Fallback: use Karma's file list to load all .spec.ts files. This works regardless of require.context availability.
+
 const allSpecFiles = Object.keys((window as any).__karma__.files)
   .filter((file: string) => /\.spec\.ts$/.test(file))
   .map((file: string) => file.replace(/^\/base\//, './'));
 
 allSpecFiles.forEach((moduleId: string) => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
+
   require(moduleId);
 });
