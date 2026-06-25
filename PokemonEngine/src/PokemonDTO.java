6397075a7b35d48a01ceb1a6e799eed492388e1c
ujusa12 <114237075+ujusa@users.noreply.github.com>
2026-06-25 06:32:34 +0530

    Add files via upload

---
+++ PokemonEngine/src/PokemonDTO.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class PokemonDTO {
+
+	Integer id ;
+	String name ;
+	Integer base_experience ;
+	Integer height ;
+	Boolean is_default ;
+	Integer order ;
+	Integer weight ;
+	List abilities ;
+	List forms ;
+	List game_indices ;
+	List held_items ;
+	String location_area_encounters ;
+	List moves ;
+	List past_types;
+	List past_abilites ;
+	List past_stats ;
+	
+}
